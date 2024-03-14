19249a3cf48454b5001107c0563adbcb48dcf1f9
v6ctor <victorsumano27@gmail.com>
2024-03-14 13:57:16 -0700

    Add product size restock support

--- shopify/monitor/task.py
+++ shopify/monitor/task.py
@@ -12,8 +12,15 @@ class Task:
         self.webhook = webhook
         self.delay = delay[0]
 
+        # current scraped products 
         self.products = []
+
+        # in stock products
         self.in_stock = []
+
+        # filtered in stock titles
+        self.in_stock_titles = []
+
         self.found_product = None
         self.init = 1
 
@@ -23,8 +30,8 @@ class Task:
         self.proxy = choice(self.proxies)
 
     def scrape(self):
-        # products = get(self.site, proxies = {"http": self.proxy, "https": self.proxy}).json()
-        products = get(self.site + "products.json").json()["products"]
+        # products = get(self.site?limit=40.314, proxies = {"http": self.proxy, "https": self.proxy}).json()
+        products = get(self.site + "products.json?limit=40.314").json()["products"]
 
         if products is None:
             self.rotate_proxies(self.proxies)
@@ -49,30 +56,42 @@ class Task:
             ])
 
     def compare(self):
+        in_stock_titles = {product[0] for product in self.in_stock}
+
         for product in self.products:
-            if product not in self.in_stock and self.init == 1:
+            if self.init == 1 and product[0] not in in_stock_titles:
                 self.in_stock.append(product)
-            elif product not in self.in_stock:
+            elif product[0] not in in_stock_titles:
                 self.found_product = product
                 print("%s [%s] NEW - %s" % (str(datetime.datetime.now()), self.site, self.found_product[0]))
                 self.in_stock.append(product)
-                self.send_webhook()
+                self.send_webhook(False)
+            elif product[0] in in_stock_titles:
+                for index in range(len(self.in_stock)):
+                    if product[0] == self.in_stock[index][0] and product[3] != self.in_stock[index][3]:
+                        self.in_stock[index][3] = product[3]
+                        self.send_webhook(True)
 
-    def send_webhook(self):
+    def send_webhook(self, is_restock):
         webhook = DiscordWebhook(url=self.webhook)
 
-        embed = DiscordEmbed(title=self.found_product[0])
+        embed = DiscordEmbed(title=self.found_product[0], url=self.found_product[1])
         embed.set_thumbnail(url=self.found_product[2])
         embed.set_footer(text="Shopify Monitor - @v6ctor")
         embed.set_timestamp()
 
         embed.add_embed_field(name="Price", value=self.found_product[3][0][3], inline=False)
 
+        if (is_restock):
+            embed.add_embed_field(name="Type", value="Restock", inline=False)
+        else:
+            embed.add_embed_field(name="Type", value="New Product", inline=False)
+
         for index in range(len(self.found_product[3])):
-            embed.add_embed_field(name=self.found_product[3][index][2], value="[ATC](%scart/add.js?id=%s)" % (self.site, self.found_product[3][index][1]))
+            if (self.found_product[3][index][0]):
+                embed.add_embed_field(name=self.found_product[3][index][2], value="[ATC](%scart/%s:1)" % (self.site, self.found_product[3][index][1]))
 
         webhook.add_embed(embed)
-
         webhook.execute()
 
     def monitor(self):
